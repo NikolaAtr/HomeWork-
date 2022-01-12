3bc5844bec1a1133482a20c64730cd28b345e4f1
NIKO_V <89060921309@mail.ru>
2022-01-13 00:36:17 +0300

    Lesson 4 Home Work (all done)

---
+++ lesson-4/Lesson-4.4/Lesson-4.4/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_4._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            // Формула Фибаначи: f(n)=f(n-1) + f(n-2), где f(0)=0, f(1)=1.
+
+
+            Console.WriteLine("Введите число для расчета ЧИСЛА ФИБАНАЧИ");
+            string StringI = Console.ReadLine();                           // Значение для расчета Фибаначи от пользоавтеля
+            int i = Convert.ToInt32(StringI);
+
+
+           
+
+            if (i > 100)
+
+                Console.WriteLine("Введите число меньше 100! ");          // Ограничим пользовательский ввод до 100
+            
+            else
+            {
+                int fib = Fibonachi(i);
+                Console.WriteLine($"{i} число Фибоначчи = {fib}");
+
+            }
+
+            int Fibonachi(int n)
+            {
+                if (n == 0 || n == 1)  // f(0)=0, f(1)=1.
+                    return n;
+
+                return Fibonachi(n - 1) + Fibonachi(n - 2); // f(n)=f(n-1) + f(n-2)
+
+
+            }
+          
+
+
+            Console.ReadKey();
+
+
+        }
+
+      
+    }
+}
---
+++ lesson-4/lesson-4.1/lesson-4.1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_4._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+           
+            string FullName1 = getFullName(); // ФИО 1.  <Пользовательский Ввод>
+            string FullName2 = getFullName(); // ФИО 2.  <Пользовательский Ввод>
+            string FullName3 = getFullName(); // ФИО 3.  <Пользовательский Ввод>
+
+            Console.WriteLine($"1. ФИО: {FullName1}");   // ВЫВАОД НА  КОНСОСЬ .. 
+            Console.WriteLine($"2. ФИО: {FullName2}");
+            Console.WriteLine($"3. ФИО: {FullName3}");
+
+            Console.ReadKey();
+        }
+
+      
+        static string getFullName()  // Метод возвращающий ФИО
+        {
+            System.Console.WriteLine("Введите Имя");
+            string firstName = Console.ReadLine();
+
+            
+            System.Console.WriteLine("Введите Фамилию");
+            string lastName = Console.ReadLine();
+
+            System.Console.WriteLine("Введите Отчество");
+            string patronymic = Console.ReadLine();
+
+            return ($"{lastName} {firstName} {patronymic}")
+                ;
+           
+
+
+        }
+
+    }
+}
