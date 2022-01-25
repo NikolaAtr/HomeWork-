8d0ebcd3f9a8d9b9e2637c3f89159ca7708268af
NIKO_V <89060921309@mail.ru>
2022-01-25 15:34:42 +0300

    lesson-7

---
+++ lesson-7/Lesson-7/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Lesson-6")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lesson-6")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("a0ee08e9-c7be-43dc-bdf6-e9216fa12e57")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
---
+++ lesson-7/Lesson-7/Program.cs
@@ -0,0 +1,32 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: Lesson_6.Program
+// Assembly: Lesson-6, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: F6B06B06-4352-4E4D-8C94-A1A1D0FD8DA1
+// Assembly location: C:\Users\89060\OneDrive\Рабочий стол\HomeWork\HomeWork-\lesson-6\Lesson-6\Lesson-6\obj\Debug\Lesson-6.exe
+
+using System;
+using System.Diagnostics;
+
+namespace Lesson_6
+{
+  internal class Program
+  {
+    private static void Main(string[] args)
+    {
+      Console.WriteLine("Список текущих процессов:     ");
+      Console.WriteLine("                              ");
+      Console.WriteLine("ID           Name Process     ");
+      Console.WriteLine("                              ");
+      foreach (Process process in Process.GetProcesses())
+        Console.WriteLine(process.Id.ToString() + "     " + process.ProcessName);
+      Console.WriteLine("                              ");
+      Console.WriteLine("Для завершения конкретногоо процесса напишите его 'ID':      ");
+      int int32 = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine("Завершить процесс " + Process.GetProcessById(int32)?.ToString() + "???");
+      Console.WriteLine("     yes / no    +  Enter  ");
+      if (Console.ReadLine() == "yes")
+        Process.GetProcessById(int32).Kill();
+      Console.ReadKey();
+    }
+  }
+}
